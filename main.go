180b095fc95b38c1eeb3075728ca3c9a2181c71c
Javier Palomo <javier.palomo.almena@gmail.com>
2020-06-12 14:05:14 +0200

    Implement a basic HTTP server with a healthcheck

--- main.go
+++ main.go
@@ -1,3 +1,25 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	log.Println("Starting greeter service ...")
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/healthz", HealthzHandler)
+
+	httpServer := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	log.Fatal(httpServer.ListenAndServe())
+}
+
+func HealthzHandler(w http.ResponseWriter, _ *http.Request) {
+	w.Write([]byte("OK"))
+}
