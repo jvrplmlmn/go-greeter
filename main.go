9f9a1f9942fe53a9dd60554e44b86f896c7b94ea
Javier Palomo <javier.palomo.almena@gmail.com>
2020-06-12 14:11:10 +0200

    Add a Greeter handler
    At this point is just says hello back

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/healthz", HealthzHandler)
+	mux.HandleFunc("/greet", GreeterHandler)
 
 	httpServer := &http.Server{
 		Addr:    ":8080",
@@ -22,4 +23,8 @@ func main() {
 
 func HealthzHandler(w http.ResponseWriter, _ *http.Request) {
 	w.Write([]byte("OK"))
+}
+
+func GreeterHandler(w http.ResponseWriter, _ *http.Request) {
+	w.Write([]byte("Hello!"))
 }
